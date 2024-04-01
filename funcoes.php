0e0f56b7aa61fc245619e087557c78cafb3d561d
dedlop <104914772+dedlop@users.noreply.github.com>
2024-04-01 18:32:28 -0300

    Funções

---
+++ funcoes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+
+        <?php
+            
+            //void (sem retorno)
+            function exibirBoasVindas() {
+                echo 'Bem-vindo ao curso de PHP!<br />';
+            }
+
+            exibirBoasVindas();
+
+            //return (com retorno)
+            function calcularAreaTerreno($largura, $comprimento) {
+                $area =  $largura * $comprimento;
+                return $area; 
+            }
+
+            $resultado =  calcularAreaTerreno(30, 50);
+            echo $resultado;
+            
+
+        ?>
+
+    </body>
+</html>
